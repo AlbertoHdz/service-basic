fdd3cc4f37a2cb1323cc03c92eb64822f4d67663
Albert Hdz <alberto_8565@hotmail.com>
2024-07-28 20:32:47 -0600

    se arreglan el error de compilar con maven

--- src/main/java/com/project/api/controller/XmlController.java
+++ src/main/java/com/project/api/controller/XmlController.java
@@ -5,7 +5,10 @@
  */
 package com.project.api.controller;
 
+import com.project.api.service.XmlService;
+import java.util.List;
 import org.json.simple.JSONValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/xml")
 public class XmlController {
     
+    @Autowired
+    XmlService xmlService;
+    
     @GetMapping(value = "/",produces = "application/json; charset=utf-8")
     public String getAllXml(){
-        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-        return xml;
+        //String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+        List xml = xmlService.getLista();
+        System.out.println("**** LISTA DE XML******");
+        System.out.println(xml.toString());
+        return xml.toString();
         //return JSONValue.toJSONString(xml);
     }
 }
--- src/main/java/com/project/api/service/XmlService.java
+++ src/main/java/com/project/api/service/XmlService.java
@@ -7,19 +7,33 @@ package com.project.api.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Alber
  */
+@Service
 public class XmlService {
-    List lista = new ArrayList();
+    ArrayList lista = new ArrayList();
     
     {
+        lista.add(new String("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
+        lista.add(new String("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
+        lista.add(new String("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
         lista.add(new String("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
     }
     
     public List getLista(){
         return lista;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder salida = new StringBuilder();
+        for(Object item : lista){
+            salida.append(item.toString());
+        }
+        return salida.toString();
     }
 }
